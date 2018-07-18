d1e4a6fa0a63643280b924e23bb51bf67bc61bc9
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-07-18 16:22:04 -0300

    0.0.9-alpha
    Animation configured for Parasite

---
+++ FPS-R/Assets/Scripts/Enemy/EnemyRandomMov.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyRandomMov : MonoBehaviour {
+
+    [SerializeField] float wanderRadius;
+    [SerializeField] float wanderTimer;
+    
+
+    private Transform target;
+    private NavMeshAgent agent;
+    Animator anim;
+    private float timer;
+    float speed = 0f;
+    bool isMoving;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isMoving = false;
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+       
+            timer = 0; 
+        }
+        if (Vector3.Distance(transform.position, agent.destination) > 1.1f)
+            speed = Mathf.Lerp(speed, 3.5f, 0.05f);
+        else
+            speed = Mathf.Lerp(speed, 0f, 0.05f);
+        
+
+        anim.SetFloat("Blend", speed);
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position; 
+    }
+}
